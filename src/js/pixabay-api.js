9044a1cc7697b11b7208e528c78c4c7a2e20d24f
svitlanasvitlyk <65176415+svitlanasvitlyk@users.noreply.github.com>
2024-11-14 20:02:38 +0100

    update pixabay

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const apiKey = '47040880-6387dd83a064ed0d7a9fde087';
 const baseUrl = 'https://pixabay.com/api/';
 
-export async function fetchImages(query, page = 1) {
+export async function fetchPhotos(query, page = 1) {
   const response = await axios.get(baseUrl, {
     params: {
       key: '47040880-6387dd83a064ed0d7a9fde087',
