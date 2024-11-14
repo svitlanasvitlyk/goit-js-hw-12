546c353dfa28a89c283f95dde4f4dadb9b890587
svitlanasvitlyk <65176415+svitlanasvitlyk@users.noreply.github.com>
2024-11-15 00:18:26 +0100

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,18 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = 'https://pixabay.com/api/';
-
-export async function fetchPhotos(query, page = 1) {
-  const response = await axios.get(baseUrl, {
-    params: {
-      key: '47040880-6387dd83a064ed0d7a9fde087',
-      q: query,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      page: page,
-      per_page: 15,
-    },
-  });
+export async function fetchPhotos(params) {
+  const response = await axios.get('https://pixabay.com/api/', { params });
+  console.log(response.config.url);
   return response.data;
 }
